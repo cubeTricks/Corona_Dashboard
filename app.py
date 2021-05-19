98a390a64a7a594286d902983e43c8ad2f7d0994
cubeTricks <39624903+cubeTricks@users.noreply.github.com>
2021-05-19 23:18:17 +0530

    Updated to latest.

--- app.py
+++ app.py
@@ -3,25 +3,20 @@ import backend_1
 
 app = Flask(__name__, static_folder='static')
 @app.route('/', methods=['GET','POST'])
-def index():
 
+
+def index():
     countries_list_for_output=[]
     countries_list=backend_1.countries_list()
-
-
+ 
     for i in countries_list:
         tmp_row={'country_name':i}
         countries_list_for_output.append(tmp_row)
         countries=countries_list_for_output
-   
-      
 
     selected_country=request.form.get('country')
-        
-  
     selected_country_data=backend_1.country_data(selected_country)
     selected_country_data_vaccine=backend_1.vaccine_data(selected_country)
-   
     
     return render_template('index.html',countries=countries,html_data=selected_country_data,html_data_vaccine=selected_country_data_vaccine)
     
