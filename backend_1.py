078bddf8ce54d76d0b5ffb9f0d600939c0948685
cubeTricks <39624903+cubeTricks@users.noreply.github.com>
2021-05-23 13:52:01 +0530

    Update backend_1.py
    Added timedelta and datetime libraries to calculate delta days based on selected date and to filter the dataframe data based on the selected dates.

--- backend_1.py
+++ backend_1.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-from datetime import timedelta
+from datetime import timedelta,datetime
 import os
 
-file_path=r'.\WHO-COVID-19-global-data_May16_db.csv'
-file_path_vaccine=r'.\vaccination-data_May16_db.csv'
+file_path=r'.\WHO-COVID-19-global-data_May23_db.csv'
+file_path_vaccine=r'.\vaccination-data_May23_db.csv'
 
 
 
@@ -13,7 +13,7 @@ df1=pd.read_csv(file_path)
 df1_vaccine=pd.read_csv(file_path_vaccine)
 column_names=df1.columns.values
 column_names_vaccine=df1_vaccine.columns.values
-#print(column_names)
+
 
 
 
@@ -34,28 +34,36 @@ def countries_list():
     return countires_list
     
     
-def country_data(selected_country):
+def country_data(selected_country,selected_date):
     df11=pd.DataFrame()
     country_name=[]
-   
-    
     country_name.append(selected_country)
     ctry_wise_df=df1[df1[metric2].isin(country_name)]
-
+    
     tmp_x_axis_dates=df1[metric1].tolist()
     tmp_x_axis_dates1=set(tmp_x_axis_dates)
     x_axis_dates_today=sorted(tmp_x_axis_dates1)
-    x_axis_dates=x_axis_dates_today[0:len(x_axis_dates_today)-1]
+    x_axis_dates=x_axis_dates_today[0:len(x_axis_dates_today)]
     c_day1=[]
-    c_day1.append(x_axis_dates[-1])
-    c_day1.append(x_axis_dates[-2])
-    c_day1.append(x_axis_dates[-3])
+    selected_date=datetime.strptime(selected_date,'%Y-%m-%d')
+    selected_date_minus_1=selected_date+timedelta(days=-1)
+    selected_date_minus_2=selected_date+timedelta(days=-2)
+    selected_date_str=selected_date.strftime('%Y-%m-%d')
+    selected_date_minus_1_str=selected_date_minus_1.strftime('%Y-%m-%d')
+    selected_date_minus_2_str=selected_date_minus_2.strftime('%Y-%m-%d')
+    c_day1.append(selected_date_str)
+    c_day1.append(selected_date_minus_1_str)
+    c_day1.append(selected_date_minus_2_str)
+    
+ 
+        
+        
 
-   
 
     df11=ctry_wise_df[ctry_wise_df[metric1].isin(c_day1)]
 
-   
+    
+    
     
     selected_country_data_for_html=[]
 
@@ -64,8 +72,7 @@ def country_data(selected_country):
         tmp_rows.append(df11.iloc[2].tolist())
         tmp_rows.append(df11.iloc[1].tolist())
         tmp_rows.append(df11.iloc[0].tolist())
-    
-        
+   
         for i in tmp_rows:
             tmp_selected_country_data_for_html_row={'Date':i[0],'Country_Name':i[2],'New_Cases':f'{i[4]:,}','Cummulative_Cases':f'{i[5]:,}','New_Deaths':f'{i[6]:,}','Cummulative_Deaths':f'{i[7]:,}'}
             selected_country_data_for_html.append(tmp_selected_country_data_for_html_row)
@@ -77,13 +84,19 @@ def vaccine_data(selected_country):
     df11_vaccine=pd.DataFrame()
     country_name_vaccine=[]
     vaccine_data_for_country={}
+   
     
     country_name_vaccine.append(selected_country)
     ctry_wise_df_vaccine=df1_vaccine[df1_vaccine[metric1_vaccine].isin(country_name_vaccine)]
+    
+ 
     if ctry_wise_df_vaccine.shape[0]>0:
+    
         i=ctry_wise_df_vaccine.iloc[0].tolist()
         
         vaccine_data_for_country={'Reported Till Date':i[4],'Country Name ':i[0],'Total Vaccines Given: ':f'{i[5]:,}','Persons Vaccinated 1+ Dose':f'{i[6]:,}','Total Vaccination Per 100 People':i[7],'Total Vaccinations of 1+ Dose per 100 ':i[8],'Types of Vaccines Used':i[9]}
-        
+    
     return vaccine_data_for_country
     
+#country_data('United States of America','2021-05-04')
+#vaccine_data('India')
